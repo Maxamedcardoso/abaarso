cc6e008dcd3e0408f2ed85c525d423a64f1149d0
Maxamedcardoso <iftiimiyahanolosha@gmail.com>
2025-03-14 12:18:29 +0300

    waxaan wax ka badal ku sameynay

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// console.log("hello world!");
+// i
+// let user= prompt(`hello world`)
+// console.log(user);
+// let x=12;
+// x=10;
+// let x=10;
+let firstname="maxamed";
+let lastname="faahir";
+let email="maxamed@example";
+let password="maxamed1123";
+let course= "html";
+var phone =404040;
+var age =22;
+const year=2023;
+const month=12;
+const day=1;
+console.log(year);
+
+
+
+
+
